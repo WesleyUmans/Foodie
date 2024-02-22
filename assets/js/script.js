14fb44948c0e229aafa81a890a9b4c3f2aad633c
Wesley <w.umans@outlook.com>
2024-02-22 21:17:54 +0100

    Added mobile nav bar and started with hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,22 @@ const menuBtn = document.querySelector(".menu-btn");
 const lineTop = document.querySelector(".line-top");
 const lineMiddle = document.querySelector(".line-middle");
 const lineBottom = document.querySelector(".line-bottom");
+const navLinks = document.querySelector(".nav-links");
+const navbarMobile = document.querySelector(".navbar-mobile");
 
 const handleMenuClick = () => {
   if (lineTop.classList.contains("active")) {
     lineTop.classList.remove("active");
     lineMiddle.classList.remove("active");
     lineBottom.classList.remove("active");
+    navLinks.classList.remove("active");
+    navbarMobile.classList.remove("active");
   } else {
     lineTop.classList.add("active");
     lineMiddle.classList.add("active");
     lineBottom.classList.add("active");
+    navLinks.classList.add("active");
+    navbarMobile.classList.add("active");
   }
 };
 
